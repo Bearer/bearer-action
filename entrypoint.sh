865164dd175d6751f7fee0b6fdde81e0cde4cb63
Simon Dawson <spdawson@gmail.com>
2023-01-18 13:50:30 +0000

    chore: Remove test output parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@
 POLICY_BREACHES=`curio scan --quiet $* .`
 SCAN_EXIT_CODE=$?
 
-# @todo FIXME:
-# [ $SCAN_EXIT_CODE -eq 0 ] || echo "policy_breaches=$POLICY_BREACHES" >>$GITHUB_OUTPUT
-
-time=`date`
-echo "time=$time" >> $GITHUB_OUTPUT
+[ $SCAN_EXIT_CODE -eq 0 ] || echo "policy_breaches=$POLICY_BREACHES" >>$GITHUB_OUTPUT
 
 exit $SCAN_EXIT_CODE
