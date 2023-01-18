41447f947ba83724158096f90705cb122d14a359
Simon Dawson <spdawson@gmail.com>
2023-01-18 12:44:12 +0000

    chore: Add debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-POLICY_BREACHES=`curio scan $* .`
+# N.B. Expect to be running in `/github/workspace`
+printf "DEBUG: Running in working directory: %s\n" `pwd`
+
+POLICY_BREACHES=`curio scan --debug $* .`
 SCAN_EXIT_CODE=$?
 
 [ $SCAN_EXIT_CODE -eq 0 ] || echo "policy-breaches=$POLICY_BREACHES" >>$GITHUB_OUTPUT
