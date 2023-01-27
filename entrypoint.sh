2dee9e6f7576caa164f2c5ab28f49d61ab08fc06
Mark Michon <mark.michon@bearer.sh>
2023-01-26 16:04:14 -0800

    chore: update policy terms to rule

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -l
 
-POLICY_BREACHES=`curio scan --quiet $* .`
+RULE_BREACHES=`curio scan --quiet $* .`
 SCAN_EXIT_CODE=$?
 
-echo "::debug::$POLICY_BREACHES"
+echo "::debug::$RULE_BREACHES"
 
 # Convert to single line
-POLICY_BREACHES="${POLICY_BREACHES//'%'/%25}"
-POLICY_BREACHES="${POLICY_BREACHES//$'\n'/%0A}"
-POLICY_BREACHES="${POLICY_BREACHES//$'\r'/%0D}"
+RULE_BREACHES="${RULE_BREACHES//'%'/%25}"
+RULE_BREACHES="${RULE_BREACHES//$'\n'/%0A}"
+RULE_BREACHES="${RULE_BREACHES//$'\r'/%0D}"
 
-echo "policy_breaches=$POLICY_BREACHES" >> $GITHUB_OUTPUT
+echo "rule_breaches=$RULE_BREACHES" >> $GITHUB_OUTPUT
 
 echo "exit_code=$SCAN_EXIT_CODE" >> $GITHUB_OUTPUT
